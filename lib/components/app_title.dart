3d1edf2faf37be79c52fea58815f148ee0198e5f
carrrieee <gcho2950@gmail.com>
2021-08-16 18:29:50 +0900

    app title 위젯 추가

---
+++ lib/components/app_title.dart
@@ -0,0 +1,14 @@
+// import 'package:bi_whitenoise/data/color.dart';
+import 'package:flutter/material.dart';
+
+Widget appTitle() {
+  return Text(
+    'MySound',
+    style: TextStyle(
+      color: Colors.white,
+      fontFamily: 'MontserratExtraBold',
+      fontWeight: FontWeight.bold,
+      fontSize: 24.0,
+    ),
+  );
+}
